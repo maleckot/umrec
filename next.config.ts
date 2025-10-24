612a32dadbba9b2144dd662ac62d831a3bced029
maleckot <joshmaniego04@gmail.com>
2025-10-25 02:49:23 +0800

    Disable ESLint during builds

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "10mb", // increase limit here
     },
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
